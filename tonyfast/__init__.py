31f9be88076442c7863af185371857bfc628a5af
tonyfast <tony.fast@gmail.com>
2022-10-06 10:49:23 -0700

    make it a module and add a post about searching notebooks

---
+++ tonyfast/__init__.py
@@ -0,0 +1,7 @@
+from midgy.run import Markdown
+
+with Markdown():
+    from . import README
+
+with __import__("importnb").Notebook(lazy=True):
+    from tonyfast import _022_10_05_dask_search as search
