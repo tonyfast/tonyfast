6b774ad6cc08fc2554b731ea1e776ef81ccfb920
tonyfast <tony.fast@gmail.com>
2022-12-31 22:22:30 -0800

    add regexes

--- tonyfast/__init__.py
+++ tonyfast/__init__.py
@@ -5,3 +5,21 @@ except ModuleNotFoundError:
 
 with Markdown():
     from . import *
+
+APPNAME = "tonyfast"
+
+
+def get_requests_cache(name=APPNAME):
+    from requests_cache import install_cache
+
+    install_cache(APPNAME, backend="filesystem", use_cache_dir=True)
+
+def load_ipython_extension(shell):
+    pass
+
+def unload_ipython_extension(shell):
+    pass
+
+import importnb    
+with importnb.Notebook():
+    from . import regexs
