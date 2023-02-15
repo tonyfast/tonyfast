d7cff9d9ac34ee8ad24d160f7871db96bed67637
tonyfast <tony.fast@gmail.com>
2023-02-15 06:01:14 +0000

    Deploying to gh-pages from @ tonyfast/tonyfast@7a1c938d718b9cf06cb2544b7fdc94f8305aed2a 🚀

--- branch/workflow-branch/run/files/mkdocs.py
+++
@@ -1,5 +0,0 @@
-from importlib import import_module
-from functools import partial
-imports = partial(import_module, package=__package__)
-with __import__("importnb").Notebook():
-    MarkdownNotebook = imports(".xxii.2022-12-23-mkdocs-plugin").MarkdownNotebook
