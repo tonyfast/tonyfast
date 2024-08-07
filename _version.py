54a6a027486ab5ce4841d1806453aca7eeb45462
tonyfast <tony.fast@gmail.com>
2024-08-07 18:28:37 +0000

    Deploying to gh-pages from @ tonyfast/tonyfast@59f43a6252cfa9747650c269e21ae70f530beacc 🚀

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g27f344d'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g27f344d')
+__version__ = version = '0.1.dev1+g59f43a6'
+__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g59f43a6')
